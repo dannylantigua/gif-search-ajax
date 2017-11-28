7b359b2a13e8cadf31fa48f91466ca82617ed014
Danny Lantigua <dannylantigua@dannys-mbp.lab.lagcc>
2017-11-27 19:46:33 -0500

    Adds css styles and search features

--- js/script.js
+++ js/script.js
@@ -1,11 +1,62 @@
-fetch("https://api.giphy.com/v1/gifs/search?q=breaking+bad&api_key=dc6zaTOxFJmzC&limit=7").then(function(response){
-  return response.json();}).then(function(response){
-  showGifs(response.data);});
+// Creating variables
+var userInput = document.querySelector('#user-gif-search');
+var searchGifs = document.querySelector('#submit-gif-search');
+var gifContainer = document.querySelector('#gif-container');
+var spanSR = document.querySelector('#search-result');
 
+// Clear contianer
+function clearContainer(){
+  while(gifContainer.firstChild)
+      gifContainer.removeChild(gifContainer.firstChild);
+
+  spanSR.innerHTML = '';
+}
+
+//Fetch search
+function doTheShearch(url){
+  fetch(url).then(function(response){
+  return response.json();console.log(response.json());}).then(function(response){
+  showGifs(response.data);});
+}
+
+//Show results on the gif-container div
 function showGifs(data){
   for(var x = 0; x < data.length; x++){
     var elem = document.createElement('img');
     elem.src = data[x].images.original.url;
-    document.body.appendChild(elem);
+    gifContainer.appendChild(elem);
   }
+}
+
+//Concat the strings and send search params
+function sendParams(){
+  clearContainer();
+  var inputSearch = userInput.value;
+  var inputResult = inputSearch.split(' ').join('+');
+
+  if (inputSearch !== ''){
+    var urlDir = "https://api.giphy.com/v1/gifs/search?q="+inputResult
+    +"&api_key=dc6zaTOxFJmzC&limit=7";
+    spanSR.innerHTML = inputSearch;
+    doTheShearch(urlDir);
+    clearFields();
+  }
+}
+
+//Clear fields
+function clearFields(){
+   userInput.value="";
+   userInput.focus();
+}
+
+//Validating the elements exist on the page
+if(userInput && searchGifs && gifContainer){
+  //Enter key event listener for input
+  userInput.addEventListener("keypress",function(e){
+      var key = e.keyCode;
+      if (key === 13){sendParams();}
+  });
+
+  //Search button click event listener
+  searchGifs.addEventListener("click", function(){sendParams();});
 }
